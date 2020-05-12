e057e51fe688373684b1e98b9229f4280985d6cb
Michał Krawczyk <michal.krawczyk10@gmail.com>
2020-05-13 00:38:07 +0200

    3.5a

---
+++ js/main.js
@@ -0,0 +1,10 @@
+alert('hejka');
+console.log('hej hej');
+
+const name = 'Michał';
+const age = 33;
+
+console.log(name);
+console.log(age);
+
+console.log(`hej, mam na imię ${name} i mam ${age} lata`);
