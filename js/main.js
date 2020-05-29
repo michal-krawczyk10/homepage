597ca52bae921e2d1291db6c9ab7a0da7722c1e1
Michał Krawczyk <michal.krawczyk10@gmail.com>
2020-05-29 11:57:52 +0200

    add js run welcome phrase

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-alert('hejka');
-console.log('hej hej');
+alert("hejka");
+console.log("hej hej");
 
-const name = 'Michał';
+const name = "Michał";
 const age = 33;
 
 console.log(name);
@@ -9,14 +9,26 @@ console.log(age);
 
 console.log(`hej, mam na imię ${name} i mam ${age} lata`);
 
-const welcomeHeader = document.querySelector('.welcome__header--js');
+const welcomeHeader = document.querySelector(".welcome__header--js");
 
 console.log(welcomeHeader);
 
-welcomeHeader.innerHTML = 'Miło że wpadłaś / wpadłeś :)';
+welcomeHeader.innerHTML = "Miło że wpadłaś / wpadłeś :)";
 
-function welcome()}
+function createContent(querySelectorContent, content) {
+	const element = document.querySelector(querySelectorContent);
+	element.innerHTML = content;
+}
 
-<h1 class="welcome__header">Mam na imię Michał i powoli ogarniam magię frontendu.</h1>
-<p class="welcome__text">Dziś mamy: data, co oznacza, że mam lata</p> 
+const myself = {
+	age: 33,
+	name: "Michał",
+};
 
+
+createContent(
+	".welcome__header--js",
+	`Hej, mam na imię ${'Michał'}, dziś jest data i lecimy z frontendem`
+);
+
+const date = console.log(myself);
